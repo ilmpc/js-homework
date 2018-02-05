9384836267da53552594a03b940414934c39f85b
Vladimir Erokhov <Vladimir.Erokhov@noveogroup.com>
2018-02-05 15:54:39 +0700

    05-dom-events tests was added

---
+++ tasks/05-dom-events/events.js
@@ -0,0 +1,33 @@
+document.body.innerHTML =
+  `<input type="button" id="btn-hide" value="Click to hide"/>
+  <div id="hide">Some beautiful text here</div>
+  <ul class="messages">
+  <li>
+  <h3>Title 1</h3>
+  <p>Some text here...</p>
+  <button class="remove-button">[x]</button>
+  <li>
+  <h3>Title 2</h3>
+  <p>Some text here...</p>
+  <button class="remove-button">[x]</button>
+  <li>
+  <h3>Title 3</h3>
+  <p>Some text here...</p>
+  <button class="remove-button">[x]</button>
+  </ul>`;
+
+/**
+ * Задача #1
+ * Скрыть элемент c id = 'hide' при нажатии на кнопку с id = 'btn-hide'
+ */
+
+/** Ваш код */
+
+/**
+ * Задача #2
+ * Дан список сообщений 'messages'. Требуется при нажатии на кнопку 'remove-button'
+ * добавлять класс hidden к родительскому элементу (li);
+ * Используйте делегирование, т.е. один обработчик для всего списка.
+ */
+
+/** Ваш код */
