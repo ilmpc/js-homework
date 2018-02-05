b0920504e3897f8c6ea63343a5cc7285cf2dfdae
Vladimir Erokhov <Vladimir.Erokhov@noveogroup.com>
2018-02-05 15:54:39 +0700

    05-dom-events tests was added

---
+++ tasks/05-dom-events/dom.js
@@ -0,0 +1,57 @@
+document.body.innerHTML =
+  `<div id="main-article">
+  </div>
+  <ul class="options-list">
+  <li>First</li>
+  <li>Second</li>
+  <li>Third</li>
+  </ul>
+  <div class="btn" style="background-color:green;">Confirm</div>
+  <ul class="orders-list">
+  <li data-state="new">Shirt</li>
+  <li data-state="pending">Tie</li>
+  <li data-state="canceled">Suit</li>
+  </ul>`;
+
+
+/**
+ * Задача #1
+ * Создать и добавить элемент с классом = 'main-title' и текстом
+ * "My personal page", как самый первый элемент в body
+ */
+
+/** Ваш код */
+
+/**
+ * Задача #2
+ * Найти элемент с id = 'main-article' и добавить в него текст,
+ * состоящий не менее чем из 50 символов
+ */
+
+/** Ваш код */
+
+/**
+ * Задача #3
+ * Удалить второй элемент из списка options-list
+ */
+
+/** Ваш код */
+
+/**
+ * Задача #4
+ * Клонировать элемент с классом 'btn', сделать его background красным,
+ * поменять текст на Cancel и добавить его на страницу вслед за исходным элементом.
+ * Подсказка: можно написать вспомогательную функцию insertAfter, т.к.
+ * существует только insertBefore
+ */
+
+/** Ваш код */
+
+/**
+ * Задача #5
+ * Написать код который заменяет все data-state из orders-list
+ * на closed
+ * (Не используйте в этой задаче dataset)
+ */
+
+/** Ваш код */
